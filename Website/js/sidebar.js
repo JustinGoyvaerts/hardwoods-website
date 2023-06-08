6760c80bae3f81384e5acd6ab1c056d2a996c4b3
JustinGoyvaerts <113667682+JustinGoyvaerts@users.noreply.github.com>
2023-06-08 23:38:47 +0200

    Add files via upload

---
+++ Website/js/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "40%";
+    document.getElementById("main").style.marginLeft = "40%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
