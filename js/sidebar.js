33cb54ea088c8539ab6f0e568d727007c4eb83e4
JustinGoyvaerts <113667682+JustinGoyvaerts@users.noreply.github.com>
2023-06-21 22:08:08 +0200

    Delete js directory

--- js/sidebar.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "40%";
-    document.getElementById("main").style.marginLeft = "40%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
